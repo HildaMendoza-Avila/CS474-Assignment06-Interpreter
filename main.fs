a8fe8d1c20c06833b92f5cd079627240f20addd8
HildaMendoza <hildabmendoza@gmail.com>
2020-12-04 20:47:41 +0000

    Added Functions

--- main.fs
+++ main.fs
@@ -24,49 +24,52 @@ let lookup (variableNameToFind, bindingsToExamine) =
 type Expression =
   | IntConstant of int32
   | BooleanConstant of bool
-  | BinOp of Operator * Expression * Expression
+  | BinaryOp of Operator * Expression * Expression
   | IfExpression of Expression * Expression * Expression
   | ComparisonExpression of ComparisonType * Expression * Expression
   | LetExpression of NameType * Expression * Expression
   | VariableExpression of NameType
+  | FunctionCallExpression of NameType * (Expression list)
+
+type FunctionType = Function of NameType  * Expression * (NameType list)
 
 let mutable currDynamicScopeEnv = DynamicScopedEnvironment([]) 
 
-let rec eval (c, e) =
+let rec eval (c, e, functions) =
   match c with
   | IntConstant(value) -> (IntValue value)
   | BooleanConstant(value) -> (BooleanValue value)
-  | BinOp(op,left,right) ->
-    let (IntValue l) = eval (left, e)
-    let (IntValue r) = eval (right, e)
+  | BinaryOp(op,left,right) ->
+    let (IntValue l) = eval (left, e, functions)
+    let (IntValue r) = eval (right, e, functions)
     match op with
     | MINUS -> IntValue (l - r)
     | PLUS -> IntValue (l + r)
     | TIMES -> IntValue (l * r)
     | DIV -> IntValue (l / r)
   | ComparisonExpression(compType, left, right) ->
-    let (IntValue l) = eval (left, e)
-    let (IntValue r) = eval (right, e)
+    let (IntValue l) = eval (left, e, functions)
+    let (IntValue r) = eval (right, e, functions)
     match compType with
     | EQ -> BooleanValue (l = r)
   | IfExpression(compExpression, thenExpression, elseExpression) ->
-    let (BooleanValue condResult) = eval (compExpression, e)
-    if condResult then eval (thenExpression, e)
-    else eval (elseExpression, e)
+    let (BooleanValue condResult) = eval (compExpression, e, functions)
+    if condResult then eval (thenExpression, e, functions)
+    else eval (elseExpression, e, functions)
   | LetExpression(name, valueExpression, body) ->
     // Bind the name to the value of evaluating the initialization expression
-    let value = eval (valueExpression, e)
+    let value = eval (valueExpression, e, functions)
     match e with
     | DynamicScopedEnvironment(outerScopeBindingsList) ->
       match currDynamicScopeEnv with
       | DynamicScopedEnvironment(bindingsList) ->
         let newBindingsList = bind (name, value, bindingsList)
         currDynamicScopeEnv <- DynamicScopedEnvironment(newBindingsList)
-        eval (body, currDynamicScopeEnv)
+        eval (body, currDynamicScopeEnv, functions)
       | _ -> BooleanValue(false)    // there was an error 
     | LexicalScopedEnvironment(bindingsList) -> 
       let newBindingsList = bind (name, value, bindingsList)
-      eval (body, LexicalScopedEnvironment(newBindingsList))
+      eval (body, LexicalScopedEnvironment(newBindingsList), functions)
 
   | VariableExpression(varName) ->
     // Check what is the value bound to the name in the variable expression
@@ -81,23 +84,23 @@ let rec eval (c, e) =
 let p1 = IntConstant(474)     
 
 // 400 + 74
-let p2 = BinOp(PLUS, IntConstant(400), IntConstant(74)) 
+let p2 = BinaryOp(PLUS, IntConstant(400), IntConstant(74)) 
 
 // 400 + (70 + 4)
-let p3 = BinOp(PLUS, IntConstant(400), BinOp(PLUS, IntConstant(70), IntConstant(4)))
+let p3 = BinaryOp(PLUS, IntConstant(400), BinaryOp(PLUS, IntConstant(70), IntConstant(4)))
 
 // 400 + true
-let p4 = BinOp(PLUS, IntConstant(400), BooleanConstant(true)) 
+let p4 = BinaryOp(PLUS, IntConstant(400), BooleanConstant(true)) 
 
 // if (474 - (400 + 74) == 0) 0 else (1 / (474 - (400 + 74)))
 let p5 = IfExpression(
   ComparisonExpression(
     EQ, 
-    BinOp(MINUS, IntConstant(474), BinOp(PLUS, IntConstant(400), IntConstant(74))), 
+    BinaryOp(MINUS, IntConstant(474), BinaryOp(PLUS, IntConstant(400), IntConstant(74))), 
     IntConstant(0)
   ), 
   IntConstant(0), 
-  BinOp(DIV, IntConstant(1), BinOp(MINUS, IntConstant(474), BinOp(PLUS, IntConstant(400), IntConstant(74))))
+  BinaryOp(DIV, IntConstant(1), BinaryOp(MINUS, IntConstant(474), BinaryOp(PLUS, IntConstant(400), IntConstant(74))))
 )
 
 // { let operand = (474 - (400 + 74));
@@ -105,7 +108,7 @@ let p5 = IfExpression(
 // }
 let p6 = LetExpression(
   Name("operand"), 
-  BinOp(MINUS, IntConstant(474), BinOp(PLUS, IntConstant(400), IntConstant(74))), 
+  BinaryOp(MINUS, IntConstant(474), BinaryOp(PLUS, IntConstant(400), IntConstant(74))), 
   IfExpression(
     ComparisonExpression(
       EQ,
@@ -113,7 +116,7 @@ let p6 = LetExpression(
       IntConstant(0)
     ), 
     IntConstant(0), 
-    BinOp(DIV, IntConstant(474), VariableExpression(Name("operand"))
+    BinaryOp(DIV, IntConstant(474), VariableExpression(Name("operand"))
     )
   )
 )
@@ -131,7 +134,7 @@ let p6 = LetExpression(
 let p7 = LetExpression(
   Name("x"), 
   IntConstant(10), 
-  BinOp(
+  BinaryOp(
     PLUS, 
     LetExpression(
       Name("x"), 
@@ -142,10 +145,85 @@ let p7 = LetExpression(
   )
 )
 
+let params = 
 
-let e = DynamicScopedEnvironment([])
+// safeDivision(top, bot) : if (bot == 0) then 0 else top/bot
+let safeDivision = Function(
+  Name("safeDivision"), 
+  IfExpression(
+    ComparisonExpression(
+      EQ, 
+      VariableExpression(
+        Name("bot")
+      ), 
+      IntConstant(0)
+    ), 
+    IntConstant(0), 
+    BinaryOp(
+       DIV, 
+       VariableExpression(
+         Name("bot")
+       ), 
+       VariableExpression(
+         Name("top")
+       )
+    )
+  ), 
+  [Name("top"); Name("bot")]
+)
 
-printfn "\ncurr result: %A" (eval(p7, e))
+// fact(x) : if (x == 1) then 1 else x * fact(x-1)
+let fact = Function(
+  Name("fact"),  
+  IfExpression(
+    ComparisonExpression(
+      EQ, 
+      VariableExpression(
+        Name("x")
+      ), 
+      IntConstant(1)
+    ), 
+    IntConstant(1), 
+    BinaryOp(
+      TIMES, 
+      VariableExpression(
+        Name("x")
+      ), 
+      FunctionCallExpression(
+        Name("fact"), 
+        [BinaryOp(
+          MINUS, 
+           VariableExpression(
+             Name("x")
+            ),
+          IntConstant(1)
+        )]
+      )
+    )
+  ), 
+  [Name("x")]
+)
+
+// safeDivision(474, (474 - (400 + 74)))
+let p8 = FunctionCallExpression(
+  Name("safeDivision"), 
+  [
+    IntConstant(474); 
+    BinaryOp(
+      MINUS, 
+      IntConstant(474), 
+      BinaryOp(
+        PLUS, 
+        IntConstant(400), 
+        IntConstant(74)
+      )
+    )
+  ]
+)
+
+let e = LexicalScopedEnvironment([])
+
+printfn "\ncurr result: %A" (eval(p8, e, [safeDivision, [fact, []]]))
 
 
 
